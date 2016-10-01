7eef66e2a253b10d96fbb6dbdaf0760cbd7d1c84
IvynaSantino <ivyna.alves@ccc.ufcg.edu.br>
2016-09-30 23:08:52 -0300

    Falta conservar a carga com a 2 colisão

--- espaco.py
+++ espaco.py
@@ -49,10 +49,38 @@ explosao_foguete = {
 	'rect': Rect(0, 0, 300, 300)
 }
 
+# COLISÃO COM CARGAS
+cargas_conserva = {
+	'surface': pygame.image.load('carga.png').convert_alpha(),
+	'position': [],
+	'speed': {
+		'x': 0,
+		'y': 0
+	},
+	'rect': Rect(0, 0, 300, 300)
+}
 
 # NOME DO JOGO: NAO SEI AINDA!
 pygame.display.set_caption('SIMULADOR DE CARGA')
 
+# CARGAS
+def criaCarga():
+	return {
+		'surface': pygame.image.load('carga.png'),
+		'position': [750, randrange(580)],
+		'speed': randrange(7)
+	}
+
+listaDeCargas = []
+
+def moverCargas():
+	for carga in listaDeCargas:
+		carga['position'][0] -= carga['speed']
+
+def removeCarga():
+	for carga in listaDeCargas:
+		if carga['position'][0] > 750:
+			listaDeCargas.remove(carga)
 
 # MOVIMENTO DE METEOROS
 def criarMeteoro():
@@ -77,7 +105,7 @@ def remove_meteoros():
 velocidade_meteoroA = 120
 
 
-# DETECTAR COLISÕES
+# DETECTAR COLISÕES: METEOROS E CARGAS
 def get_rect(obj):
 	return Rect(obj['position'][0],
 				obj['position'][1],
@@ -91,6 +119,15 @@ def foguete_colisao():
 		if foguete_rect.colliderect(get_rect(meteoro)):
 			return True
 	return False
+
+def cargaColisao():
+	foguete_rect = get_rect(foguete)
+	
+	for carga in listaDeCargas:
+		if foguete_rect.colliderect(get_rect(carga)):
+			return True
+		return False
+		
 
 # INICIANDO A CARGA DO FOGUETE
 cargaInicial = 10.0
@@ -109,6 +146,9 @@ def conservacaoDeCargas(cargaInicial):
 clock = pygame.time.Clock()
 collided = False
 collision_animation_count = 0
+colisao_animation_carga = 0
+velocidade_carga = 120
+colisaoCarga = False
 
 while True:
 	
@@ -121,13 +161,20 @@ while True:
 		if event.type == QUIT:
 			exit()
 	
-	print conservacaoDeCargas(cargaInicial)
+	#print conservacaoDeCargas(cargaInicial)
 			
 	if not velocidade_meteoroA:
 		velocidade_meteoroA = 120
 		listaMeteoros.append(criarMeteoro())
 	else:
 		velocidade_meteoroA -= 1
+		
+		
+	if not velocidade_carga:
+		velocidade_carga = 120
+		listaDeCargas.append(criaCarga())
+	else:
+		velocidade_carga -= 1
 		
 		
 	# COMANDOS ATRAVÉS DAS TECLAS
@@ -153,10 +200,30 @@ while True:
 	# MOVIMENTO DO FOGUETE E DETECTAR COLISÕES
 	if not collided:
 		collided = foguete_colisao()
+		
 		foguete['position'][0] += foguete['speed']['x']
 		foguete['position'][1] += foguete['speed']['y']
 		tela.blit(foguete['surface'], foguete['position'])
+		colisaoCarga = cargaColisao()
 		
+		if colisaoCarga:
+			
+			while True:
+				if colisao_animation_carga == 2:
+					
+					colisaoCarga = cargaColisao()
+					cargaInicial = conservacaoDeCargas(cargaInicial)
+					print cargaInicial
+					break
+				else:
+					cargas_conserva['rect'].x = colisao_animation_carga * 50
+					cargas_conserva['position'] = foguete['position']
+					
+					colisao_animation_carga += 1		
+			
+			colisao_animation_carga = 0
+			colisaoCarga = False
+			
 		# NÃO ULTRAPASSAR A TELA
 		if foguete['position'][0] > 550:
 			foguete['position'][0] -= 10
@@ -173,21 +240,28 @@ while True:
 		if collision_animation_count == 3:
 			text = game_font.render('GAME OVER', 1, (255, 0, 0))
 			tela.blit(text, (150, 250))
+			
 		else:
 			#FRAME DA EXPLOSÃO
 			explosao_foguete['rect'].x = collision_animation_count * 300
 			explosao_foguete['position'] = foguete['position']
 			tela.blit(explosao_foguete['surface'], explosao_foguete['position'], explosao_foguete['rect'])
 			collision_animation_count += 1
+			print cargaInicial
 			
-	# INICIAR FOGUETE NA POSIÇÃO 40 x 300 NA TELA
 	
 	mover_meteoros()
+	moverCargas()
 	
 	for meteoro in listaMeteoros:
 		tela.blit(meteoro['surface'], meteoro['position'])
 		
+	for carga in listaDeCargas:
+		tela.blit(carga['surface'], carga['position'])
+	
 	pygame.display.update()
 	tempo = clock.tick(20)
 	
 	remove_meteoros()
+	removeCarga()
+
